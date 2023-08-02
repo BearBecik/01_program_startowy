8d542b5eb989298c1711d9f1871f733e6c6ceb32
Arek B <Arkadiusz.Bett@gmail.com>
2023-08-02 18:24:13 +0200

    Zadnie Dzien 4

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿string name = "Ewa";
+string gender = "K";
+int age = 30;
+
+if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine($"mam na imę {name} w wieku {age} lat");
+}
+if (gender == "K" && age < 30)
+{
+    Console.WriteLine($"Jestem Kobietą poniżej {age} lat");
+}
+else
+{
+    Console.WriteLine($"Nie jestem Kobietą poniżej {age} lat");
+}
+// cz.II zmiana płci
+name = "Arek";
+gender = "M";
+age = 17;
+if (gender == "M" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mężczyzna");
+}
+else if (gender == "M")
+{
+    Console.WriteLine($"Mężczyzną o imieniu {name}");
+}
